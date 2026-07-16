c8c3ad2ff6d7524257c0075a7f39fde030e465e6
Andre Wilkson <wilksontec@gmail.com>
2026-07-16 14:17:45 -0300

    perf: otimizacoes invisiveis de LCP, lazy load e cache na LP

--- server.js
+++ server.js
@@ -8,7 +8,9 @@ const app = express();
 const PORT = process.env.PORT || 4000;
 
 app.set('view engine', 'ejs');
-app.use(express.static('public'));
+app.use(express.static('public', {
+    maxAge: '30d'
+}));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
